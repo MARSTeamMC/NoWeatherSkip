b95ac0c1ec4fe04740844468ff48be4d9c2342bf
MDen <danilalikan@gmail.com>
2026-05-29 21:14:01 +0500

    feat: port to 26.1

--- src/main/java/net/mars/noweatherskip_mt/mixin/ServerLevelMixin.java
+++ src/main/java/net/mars/noweatherskip_mt/mixin/ServerLevelMixin.java
@@ -1,24 +1,25 @@
 package net.mars.noweatherskip_mt.mixin;
 
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.level.storage.ServerLevelData;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.world.level.saveddata.WeatherData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
 
 @Mixin(ServerLevel.class)
 public abstract class ServerLevelMixin {
     @Shadow
-    @Final
-    private ServerLevelData serverLevelData;
+    abstract WeatherData getWeatherData();
 
-    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;resetWeatherCycle()V"))
-    private void redirectResetWeatherCycle(ServerLevel instance) {
-        if (this.serverLevelData.isThundering()) {
-            this.serverLevelData.setThundering(false);
-            this.serverLevelData.setThunderTime(0);
-        }
+    @Inject(method = "Lnet/minecraft/server/level/ServerLevel;resetWeatherCycle()V", at = @At("HEAD"), cancellable = true)
+    private void onResetWeatherCycle(CallbackInfo ci) {
+        WeatherData weatherData = this.getWeatherData();
+        weatherData.setThunderTime(0);
+        weatherData.setThundering(false);
+        ci.cancel();
     }
 }
